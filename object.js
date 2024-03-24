c4e04de1e977e4e10ad0352590b9c12e8856e164
bushra11771 <bushra11771@gmail.com>
2024-03-24 10:34:42 +0500

    function code added

--- object.js
+++ object.js
@@ -24,3 +24,50 @@ var company = {
   };
   console.log(company)
   
+
+
+    // FUNCTION 
+    // function are first class data 
+    // functions are object 
+function multiply(x, y) {
+  return x+y;
+}
+
+console.log(multiply(5,3));
+multiply.version = "v.1.0.0"
+console.log(multiply)
+console.log(multiply.version)
+
+
+  //  Function factory 
+  function makeMultiplier(multiplier)  {
+    var myFunc = function (x) {
+      return multiplier + x;
+    };
+    return myFunc;
+
+  }
+
+
+  var multiplyBy3 = makeMultiplier(3);
+  console.log(multiplyBy3(10));
+  var doubleAll = makeMultiplier(2);
+  console.log(doubleAll(100))
+
+
+
+
+  // passing functio as Parameters 
+  function doOprationOn(x, operation) 
+  {
+    return operation(x)
+  }
+
+  var result = doOprationOn(5, multiplyBy3);
+  console.log(
+    result
+  )
+
+  result = doOprationOn(100, doubleAll);
+  console.log(result)
+
